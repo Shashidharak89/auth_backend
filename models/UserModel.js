2b641a9720cf260e6b55f936f999f303eb0045ef
Shashidhara K <shashidharak334@gmail.com>
2025-01-17 19:09:52 +0530

    Add files via upload

---
+++ models/UserModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+// Creating post schema using Mongoose Schema class
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    coins:{
+      type:Number,
+      default:200,
+    }
+  },
+  { timestamps: true }
+);
+
+// Creating a model from schema
+const User = mongoose.model("User", UserSchema);
+
+export default User;
---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import { postsRoutes } from "./routes/postsRoutes.js";
+import { usersRoutes } from "./routes/usersRoutes.js";
+import cors from "cors";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const mongo_url=process.env.MONGO_URL;
+port=process.env.PORT;
+
+// Initializing Express app
+const app = express();
+app.use(cors());
+// Middleware to receive JSON
+app.use(express.json());
+
+// Adding the API end-points and the route handlers
+app.use("/api/posts", postsRoutes);
+app.use("/api/users", usersRoutes);
+
+// Connecting to MongoDB using Mongoose
+mongoose
+  .connect(mongo_url, { dbName: "demo_db" })
+  .then(() => {
+    console.log("connected to DB successfully");
+    
+    // Listening to requests if DB connection is successful
+    app.listen(PORT, "localhost", () => console.log("Listening to port ",PORT));
+  })
+  .catch((err) => console.log(err));
